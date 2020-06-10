37b36e80ac97d0cc86890197062dce451cd15113
juser0719 <sinsa0719@naver.com>
2020-06-10 17:04:25 +0900

    project_update

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,7 +11,7 @@ class Projects extends Component {
      if(tab_state === 0){
         return(
         <div className = "project-grid">
-            {/* project1 */}
+           {/* React project1 */}
            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://cdn.jootc.com/wp-content/uploads/2020/03/react-logo-gray.png) center / cover'}}>
                    React project #1
@@ -27,7 +27,7 @@ class Projects extends Component {
                    <IconButton name = "share" style = {{color : '#fff'}}/>
                </CardMenu>
            </Card>
-           {/* project2 */}
+           {/* React project2 */}
            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://cdn.jootc.com/wp-content/uploads/2020/03/react-logo-gray.png) center / cover'}}>
                    React project #2
@@ -43,7 +43,7 @@ class Projects extends Component {
                    <IconButton name = "share" style = {{color : '#fff'}}/>
                </CardMenu>
            </Card>
-           {/* project3 */}
+           {/* React project3 */}
            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://cdn.jootc.com/wp-content/uploads/2020/03/react-logo-gray.png) center / cover'}}>
                    React project #3
@@ -63,15 +63,162 @@ class Projects extends Component {
         )
     }else if (tab_state === 1){
         return(
-            <div><h1>Android Tab</h1></div>
+            <div className = "project-grid">
+            {/* Android project1 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://ak7.picdn.net/shutterstock/videos/31474717/thumb/1.jpg) center / cover'}}>
+                Android project #1
+                </CardTitle>
+                <CardText>
+                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                     <Button colored>GitHub</Button>
+                     <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+            {/* Android project2 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://ak7.picdn.net/shutterstock/videos/31474717/thumb/1.jpg) center / cover'}}>
+                Android project #2
+                </CardTitle>
+                <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                 <Button colored>GitHub</Button>
+                 <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+            {/* Android project3 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://ak7.picdn.net/shutterstock/videos/31474717/thumb/1.jpg) center / cover'}}>
+                Android project #3
+                </CardTitle>
+                <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                 <Button colored>GitHub</Button>
+                 <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+         </div>
         )
     }else if (tab_state === 2){
         return(
-            <div><h1>Computer System Tab</h1></div>
+            <div className = "project-grid">
+            {/* SW project1 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://img.extrememanual.net/2018/04/cpu_info_title_01-370x245.jpg) center / cover'}}>
+                Computer System project #1
+                </CardTitle>
+                <CardText>
+                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                     <Button colored>GitHub</Button>
+                     <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+            {/* SW project2 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://img.extrememanual.net/2018/04/cpu_info_title_01-370x245.jpg) center / cover'}}>
+                Computer System project #2
+                </CardTitle>
+                <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                 <Button colored>GitHub</Button>
+                 <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+            {/* SW project3 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://img.extrememanual.net/2018/04/cpu_info_title_01-370x245.jpg) center / cover'}}>
+                Computer System project #3
+                </CardTitle>
+                <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                 <Button colored>GitHub</Button>
+                 <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+         </div>
         )
     }else if (tab_state === 3){
         return(
-            <div><h1>Algorithm Tab</h1></div>
+            <div className = "project-grid">
+            {/* Alorithm project1 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://cdn1.vectorstock.com/i/1000x1000/20/10/algorithm-outline-blue-icon-on-dark-vector-18592010.jpg) center / cover'}}>
+                Alorithm project #1
+                </CardTitle>
+                <CardText>
+                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                     <Button colored>GitHub</Button>
+                     <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+            {/* Alorithm project2 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://cdn1.vectorstock.com/i/1000x1000/20/10/algorithm-outline-blue-icon-on-dark-vector-18592010.jpg) center / cover'}}>
+                Alorithm project #2
+                </CardTitle>
+                <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                 <Button colored>GitHub</Button>
+                 <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+            {/* Alorithm project3 */}
+            <Card shadow={5} style = {{minWidth: '450', margin:'auto'}}>
+                <CardTitle style = {{color: '#fff', height: '176px', background:'url(https://cdn1.vectorstock.com/i/1000x1000/20/10/algorithm-outline-blue-icon-on-dark-vector-18592010.jpg) center / cover'}}>
+                Alorithm project #3
+                </CardTitle>
+                <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                </CardText>
+                <CardActions border>
+                 <Button colored>GitHub</Button>
+                 <Button colored>Live_Demo</Button>
+                </CardActions>
+                <CardMenu style ={{color : '#ff'}}>
+                    <IconButton name = "share" style = {{color : '#fff'}}/>
+                </CardMenu>
+            </Card>
+         </div>
         )
     }   
     }
